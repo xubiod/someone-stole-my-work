41f2f50361074f7fda49fb0a997abb3b206cf7a1
Xubiod <xubiod.mail@gmail.com>
2025-12-08 04:37:19 -0500

    Initial commit (by create-cloudflare CLI)
    Details:
      C3 = create-cloudflare@2.58.0
      project name = someone-stole-my-work
      package manager = npm@11.6.2
      wrangler = wrangler@4.53.0
      git = 2.50.1 (Apple Git-155)

---
+++ src/worker.js
@@ -0,0 +1,86 @@
+const mapping = {
+  "originalURL": "Original work URL",
+  "postURL": "Infringing URL",
+  "mustBeHolder": "Must be reported by rights holder/creator"
+}
+
+export default {
+  async fetch(request, env, ctx) {
+    let r = ""
+    let toc = ""
+
+		const requestURL = new URL(request.url)
+		const path = requestURL.pathname.toLowerCase()
+    
+		const redirect_map = env.ev_paths
+    
+    for (const block of redirect_map) {
+      let sub = `<h2 id=${block.service}>${block.service}</h2>`
+      toc = `${toc}<li><a href="#${block.service}">On ${block.service}</a></li>`
+
+      if (block.requirements) {
+        sub = `${sub}<p>You will need on hand:<ul>`
+        for (const requirement of block.requirements) {
+          sub = `${sub}<li>${mapping[requirement] ?? requirement}</li>`
+        }
+        sub = `${sub}</ul></p>`
+      }
+
+      if (block.instructions) {
+        sub = `${sub}<p>${block.instructions}</p>`
+      }
+
+      if (block.reportURL) {
+        sub = `${sub}<p><a href="${block.reportURL}" rel="noopener noreferrer">Report form</a></p>`
+      }
+
+      if (block.supportPage) {
+        sub = `${sub}<p><a href="${block.supportPage}" rel="noopener noreferrer">Support page</a></p>`
+      }
+
+      r = `${r}${sub}<br />`
+    }
+
+    toc = `<ul>${toc}</ul>`
+
+    r = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>Someone stole my work!</title>
+    <meta name="description" content="A collection to platforms with copyright report links to take back what is yours!" />
+    <meta name="author" content="Xubiod" />
+    <meta name="creator" content="Xubiod" />
+    
+    <meta property="og:title" content="Someone stole my work!"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:description" content="A collection of platforms with copyright report links to take back what is yours!" />
+    
+    <meta property="twitter:title" content="Someone stole my work!"/>
+    <meta property="twitter:description" content="A collection of platforms with copyright report links to take back what is yours!" />
+    <style>
+    html { 
+      font-family: sans-serif; 
+    }
+    </style>
+    </head>
+    <body>
+    <h1>Someone stole my work!</h1>
+    <p>Theft is very ICKY, and you can use copyright to get back what is yours!</p>
+    <p><em>Do note that copyright takedowns are a legal process, and as such you will need your legal information in many cases to make claims.</em></p>
+      ${toc}
+      <hr />
+      ${r}
+      <footer>
+        Compiled together by Xubiod.
+      </footer>
+    </body>
+    </html>`
+
+    let response = new Response(r)
+
+    response.headers.set("content-type", "text/html")
+
+    return response
+  }
+};
